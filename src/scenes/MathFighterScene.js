09456062965b4268b42c91f95dc19d1d9d51b95d
Arji Maulana <lany.lvien75@gmail.com>
2023-05-27 07:22:47 +0700

    First Commit

---
+++ src/scenes/MathFighterScene.js
@@ -0,0 +1,15 @@
+import Phaser from "phaser";
+
+export default class MathFighterScene extends Phaser.Scene {
+  constructor() {
+    super("math-fighter-scene");
+  }
+  init() {}
+  preload() {
+    this.load.image("background", "images/bg_layer1.png");
+  }
+  create() {
+    this.add.image(240, 320, "background");
+  }
+  update() {}
+}
